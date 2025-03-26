ec96cf4a9f506b26c47c40b663254615adac1a73
hyojin shin <dsvit100hjin@gmail.com>
2025-03-26 10:33:42 +0900

    User modeling/migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    # 장고에서 제공하는 user 틀을 가져옴, 받아올 값을 추가하려면 아래에 추가할 수 있음
+    pass
--- auth/settings.py
+++ auth/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 모든 시스템에 내가 만든 USER시스템을 이용해줘
+AUTH_USER_MODEL = 'accounts.User' 
