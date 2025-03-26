a2b56e387f5da795f60cefad066458a62dc4f46a
hyojin shin <dsvit100hjin@gmail.com>
2025-03-26 15:31:50 +0900

    login 인자 처리

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from articles.forms import ArticleForm
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -10,6 +11,9 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+@login_required
+# 아래에 있는 함수를 실행하기 전에 위의 함수를 먼저 실행해 주세요.
+# login이 안 되면 아래의 함수가 실행 X
 def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
